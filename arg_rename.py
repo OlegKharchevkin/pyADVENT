1913b05f089a5f486ed4812bbaaec67eb2d36ee4
oleg <harchevkin@outlook.com>
2023-11-08 23:16:05 +0300

    act_parser

--- arg_rename.py
+++ arg_rename.py
@@ -1,12 +1,12 @@
 def arg_rename(cmd_line: list, synonyms: dict):
     for index, i in enumerate(cmd_line[0]):
         if i[0] in ('o', 'a', 'h', 't', 'p', 'f'):
-            if i[0] != '':
+            if i[2] != '' and i[2] != '****':
                 cmd_line[0][index][2] = synonyms[i[2][:4]]
         if i[0] == 'w':
             for number, j in enumerate(i[2:]):
                 cmd_line[0][index][number + 2] = synonyms[j[:4]]
     for index, i in enumerate(cmd_line[1]):
         if i[0] in ('d', '*', 'c', 'a', 'n', 't'):
-            if i[0] != '':
+            if i[1] != '':
                 cmd_line[1][index][1] = synonyms[i[1][:4]]
