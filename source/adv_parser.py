35d339a0f31480fbee78228379f6153f37a15df8
oleg <harchevkin@outlook.com>
2023-11-16 10:53:43 +0300

    установщик для windows, bugfix, версия 1.4

--- source/adv_parser.py
+++ source/adv_parser.py
@@ -249,14 +249,14 @@ def obj_parser(file: str, synonyms: dict[list]):
                 buf = [j[:4].lower() for j in i[3:-1].split()]
                 for j in buf:
                     synonyms[j] = buf[0]
-                objects[buf[0]] = ["", treasure, False, [999], 0]
+                objects[buf[0]] = ["", treasure, True, [999], 0]
             elif i[0] == "i":
                 if i[3] != ">":
                     objects[buf[0]][0] = i[3:-1]
             elif i[0] == "f":
+                objects[buf[0]][2] = True
                 objects[buf[0]][3] = [int(j) for j in i[3:-1].split()]
             elif i[0] == "l":
-                objects[buf[0]][2] = True
                 objects[buf[0]][3] = [int(j) for j in i[3:-1].split()]
             elif i[0] == "s":
                 objects[buf[0]][4] = int(i[3:-1])
